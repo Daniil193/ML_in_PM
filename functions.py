92fc8f80fddb0b69070a9ccce00b816552a51f13
Daniil193 <sda193@mail.ru>
2020-07-09 08:41:56 +0300

    fix errors for data preparing

--- functions.py
+++ functions.py
@@ -17,33 +17,33 @@ class Transaction():
         self.df = DataFrame.copy()
         self.tr_col_name = col_transact_name
         self.time_col_name = col_time_name
-        self.id_col_name = col_identifier_name
         self.t_date = pd.to_datetime('2060-01-01 00:00:00.00')
         self.df.rename(columns={col_identifier_name:"case_name"}, inplace=True)
+        self.id_col_name = "case_name"
         if type(self.df[self.time_col_name][self.df.index[0]]) == str:
             self.df[self.time_col_name] = pd.to_datetime(self.df[self.time_col_name])
         
     def group_case_by_time(self):
-        self.df =  self.df.groupby(self.id_col_name).apply(lambda x: x.sort_values(self.time_col_name))
+        self.df = self.df.set_index([self.id_col_name, self.time_col_name]).sort_index()
+        self.df = self.df.reset_index()
     
     def shift_process_col(self):
-        self.df['concept:name:2'] = pd.DataFrame(self.df.groupby(self.id_col_name).apply(lambda x: \
-                         x[self.tr_col_name].shift(periods=-1).fillna('Конец лога')))[self.tr_col_name].values
+        self.df['name_2'] = self.df.groupby(self.id_col_name)[self.tr_col_name].shift(periods=-1, fill_value = 'Конец лога')
     
     def shift_time_col(self):
-        self.df['time:timestamp:2'] = pd.DataFrame(self.df.groupby(self.id_col_name).apply(lambda x: \
-                         x[self.time_col_name].shift(periods=-1).fillna(self.t_date)))[self.time_col_name].values
+        self.df['time_2'] = self.df.groupby(self.id_col_name)[self.time_col_name].shift(periods=-1, fill_value = self.t_date)
     
     def time_diff(cls):
-        if 'time:timestamp:2' not in cls.df.columns:
+        if 'time_2' not in cls.df.columns:
             cls.shift_time_col()
-        cls.df['time_diff'] = cls.df['time:timestamp:2'] - cls.df[cls.time_col_name]
-        cls.df['time_diff'] = cls.df['time_diff'].apply(lambda x: 0 if x.days > 1000 else x.total_seconds())
+        cls.df['time_diff'] = cls.df['time_2'] - cls.df[cls.time_col_name]
+        cls.df['time_diff'] = cls.df['time_diff'].dt.total_seconds()
+        cls.df.loc[cls.df['time_diff'] > 200000000, 'time_diff'] = 0
     
     def transact(cls):
-        if 'concept:name:2' not in cls.df.columns:
+        if 'name_2' not in cls.df.columns:
             cls.shift_process_col()
-        cls.df['transact'] = cls.df[cls.tr_col_name]+'-->'+cls.df['concept:name:2']
+        cls.df['transact'] = cls.df[cls.tr_col_name].astype('str')+'-->'+cls.df['name_2'].astype('str')
     
     def preparing(cls):
         cls.group_case_by_time()
@@ -73,8 +73,7 @@ class Prepare(Transaction):
     def get_result(cls):
         cls.add_init_process()
         cls.df = cls.preparing()
-        cls.df['time_diff'] = cls.df[['time_diff', 'transact']].apply(lambda x: \
-                                    0 if 'Начало лога' in x.transact else x.time_diff, axis=1)
+        cls.df.loc[cls.df['transact'].str.contains('Начало лога'), 'time_diff'] = 0
         return cls.df
 
 
@@ -161,97 +160,232 @@ class Select_cluster():
         for_draw = self.df[self.df["case_name"].isin(ids)]
         print(f"Уникальных идентификаторов = {for_draw['case_name'].unique().shape[0]}")
         return for_draw
+
+
+class Frequency_graph():
+    """
+    DataFrame - с 3 столбцами ['case_name', 'transact', 'time_diff'].
+    count_treshold - принимает на вход:
+        - string: "All" (отрисовываются ребра любой частотности)
+        - integer: 0, 10, 123,... (отрисовываются ребра с частотой до или с заданного диапазона,
+                                    указывается совместно с параметром less_or_more) 
+    less_or_more - принимает на вход:
+        - string: "<" или ">" (указывается для задания необходимого порога, если параметр 
+                                count_treshold принимает значение integer)
+    Функция возвращает граф, где цифрами обозначена частота перехода между двумя событиями.
+                             Чем ярче и толще линия, тем чаще встречается данный переход.
+    """
+    def __init__(self, DataFrame, filename = None):
+        self.df = DataFrame
+        self.name = filename
+        self.counts = None
+        self.graph = None
+    
+    def check_colon(self):
+        name_processes = tuple(self.df['transact'].unique())
+        counts = [1 for i in name_processes if ':' in i]
+        if len(counts) != 0:
+            print("""В именах событий содержится двоеточие, данный вывод некорректен, 
+                     перед отрисовкой графа необходимо заменить двоеточие на тире""")
+    
+    def graph_init(self):
+        if self.name is None:
+            curr_time = datetime.now().strftime("%H:%M:%S").replace(':', '-')
+            self.graph = Digraph('finite_state_machine', filename=f'Frequency_graph_{curr_time}')
+        else:
+            self.graph = Digraph('finite_state_machine', filename=self.name)
+        self.graph.attr(rankdir='T', size='8,5')
+        self.graph.attr('node', shape='box', style='filled', color='deepskyblue')
+        self.graph.node('Начало лога', shape='doublecircle', color='deepskyblue1')
+        self.graph.node('Конец лога', shape='doublecircle', color='brown3')
+        self.graph.attr('node', shape='box', color='lightblue')
     
     
+    def stat_calculate(self, count_treshold, less_or_more):
+        if count_treshold == 'All':
+            self.counts = pd.DataFrame(self.df['transact'].value_counts()).reset_index()
+        else:
+            try:
+                if less_or_more == "<":
+                    counts = pd.DataFrame(self.df['transact'].value_counts()).reset_index()
+                    self.counts = counts[counts['transact'] < count_treshold]
+                elif less_or_more == ">":
+                    counts = pd.DataFrame(self.df['transact'].value_counts()).reset_index()
+                    self.counts = counts[counts['transact'] > count_treshold]
+                else:
+                    return 'Недопустимое значение для параметра less_or_more или неправильно указано значение All'
+            except Exception as e:
+                print(e)
+                print("""Параметр count_treshold принимает либо значение All, либо  должно иметь тип integer,\
+                совместно с параметром less_or_more, который принимает одно из следующих значений: "<" или ">" """)
     
-def secondsToText(secs):
+    @staticmethod
+    def change_color_freq(count_transact, stat):
+        if count_transact <= stat[0]:
+            color = 'bisque3'#'brown'
+        elif (count_transact > stat[0]) and (count_transact <= stat[1]):
+            color = 'bisque3'#'coral1'
+        elif (count_transact > stat[1]) and (count_transact <= stat[2]):
+            color = 'bisque3'#'goldenrod'
+        elif (count_transact > stat[2]) and (count_transact <= stat[3]):
+            color = 'deepskyblue1'
+        elif count_transact > stat[3]:
+            color = 'cyan'
+        return color
     
-#     secs = int(np.median(list_time))
-    if secs > 1:
-        days = round(secs//86400)
-        hours = round((secs - days*86400)//3600)
-        minutes = round((secs - days*86400 - hours*3600)//60)
-        seconds = round(secs - days*86400 - hours*3600 - minutes*60)
-        result = ("{}d:".format(days) if days else "") + \
-        ("{}h:".format(hours) if hours else "") + \
-        ("{}m:".format(minutes) if minutes else "") + \
-        ("{}s:".format(seconds))
-    else:
-        result = str(round(secs, 4)) +' '+ 'sec'
+    @staticmethod
+    def change_width_freq(count_transact, stat):
+        if count_transact <= stat[0]:
+            width = '1'
+        elif (count_transact > stat[0]) and (count_transact <= stat[1]):
+            width = '1'#'2'
+        elif (count_transact > stat[1]) and (count_transact <= stat[2]):
+            width = '1'#'3'
+        elif (count_transact > stat[2]) and (count_transact <= stat[3]):
+            width = '4'
+        elif count_transact > stat[3]:
+            width = '5'
+        return width
     
-    return result
-
-
-
-def change_width(secs):
-    if secs <= 1:
-        width = '1'
-    elif (secs > 1) and (secs <= 60):
-        width = '2'
-    elif (secs > 60) and (secs <= 3600):
-        width = '3'
-    elif (secs > 3600) and (secs <= 36000):
-        width = '4'
-    elif secs > 36000:
-        width = '5'
-    return width
-
-
-
-def change_color(secs):
-    if secs <= 1:
-        color = 'cyan'
-    elif (secs > 1) and (secs <= 60):
-        color = 'deepskyblue1'
-    elif (secs > 60) and (secs <= 3600):
-        color = 'goldenrod'
-    elif (secs > 3600) and (secs <= 36000):
-        color = 'coral1'
-    elif secs > 36000:
-        color = 'brown'
-    return color
-
-
-
-def change_width_freq(count_transact, stat):
-    if count_transact <= stat[0]:
-        width = '1'
-    elif (count_transact > stat[0]) and (count_transact <= stat[1]):
-        width = '1'#'2'
-    elif (count_transact > stat[1]) and (count_transact <= stat[2]):
-        width = '1'#'3'
-    elif (count_transact > stat[2]) and (count_transact <= stat[3]):
-        width = '4'
-    elif count_transact > stat[3]:
-        width = '5'
-    return width
-
-
-def change_color_freq(count_transact, stat):
-    if count_transact <= stat[0]:
-        color = 'bisque3'#'brown'
-    elif (count_transact > stat[0]) and (count_transact <= stat[1]):
-        color = 'bisque3'#'coral1'
-    elif (count_transact > stat[1]) and (count_transact <= stat[2]):
-        color = 'bisque3'#'goldenrod'
-    elif (count_transact > stat[2]) and (count_transact <= stat[3]):
-        color = 'deepskyblue1'
-    elif count_transact > stat[3]:
-        color = 'cyan'
-    return color
-
-
-def get_stat_freq(freq_to_int):
+    @staticmethod
+    def get_stat_freq(freq_to_int):
+        percent_25 = int(np.percentile(freq_to_int, 25))
+        percent_50 = int(np.percentile(freq_to_int, 50))
+        percent_75 = int(np.percentile(freq_to_int, 75))
+        percent_95 = int(np.percentile(freq_to_int, 95))
+        stat_percent = [percent_25, percent_50, percent_75, percent_95]
+        return stat_percent
     
-    percent_25 = int(np.percentile(freq_to_int, 25))
-    percent_50 = int(np.percentile(freq_to_int, 50))
-    percent_75 = int(np.percentile(freq_to_int, 75))
-    percent_95 = int(np.percentile(freq_to_int, 95))
-    
-    stat_percent = [percent_25, percent_50, percent_75, percent_95]
-    
-    return stat_percent
+    def draw_freq(cls, count_treshold = 'All', less_or_more = None):
+        cls.stat_calculate(count_treshold, less_or_more)
+        cls.graph_init()
+        cls.check_colon()
+        
+        cls.counts.columns = ['transact', 'counts']
+        transact = cls.counts['transact'].values
+        countss = cls.counts['counts'].values
+        
+        for c in range(len(transact)):
+            stat_percent = get_stat_freq(countss)
+            tr = transact[c]
+            count = int(countss[c])
+            start = tr.split('-->')[0]
+            end = tr.split('-->')[1]
+            cls.graph.edge('{0}'.format(start), 
+                   '{0}'.format(end), 
+                   label='{0}'.format(count), 
+                   arrowhead='vee', 
+                   penwidth=change_width_freq(count, stat_percent), 
+                   color = change_color_freq(count, stat_percent), 
+                   fontcolor=change_color_freq(count, stat_percent))
+        cls.graph.view()
 
+        
+class Performance_graph(Frequency_graph):
+    """
+    DataFrame с 3 столбцами ['case_name', 'transact', 'time_diff'].
+    time_treshold - временнОй порог для отрисовки:
+        - string: "All" (отрисовка ребер любой длительности)
+        - integer: 0, 10, 123,... (отрисовка ребер с событиями, между которыми время исполнения больше или 
+                                   меньше заданного, указывается в часах, совместно с параметром less_or_more) 
+    less_or_more - принимает на вход:
+        - string: "<" или ">" (указывается для задания необходимого порога)
+    type_value - тип значения времени:
+        - string: "min","max", "median" (какой тип применить для расчета времени)
+    Функция возвращает граф, где на ребрах обозначены время исполнения перехода между двумя событиями.
+                             Чем темнее и толще линия, тем дольше исполняется данный переход.
+    """
+    def __init__(self, DataFrame, filename = None):
+        super().__init__(DataFrame, filename = None)
+        self.med_time = None
+    
+    def time_calculate(self, time_treshold, type_value, less_or_more):
+        if time_treshold == 'All':
+            self.med_time = pd.DataFrame(self.df.groupby('transact')['time_diff'].agg(['min', 
+                                                                                      'max', 
+                                                                                      'median'])).reset_index()
+        else:
+            try:
+                if less_or_more == "<":
+                    med_time = pd.DataFrame(self.df.groupby('transact')['time_diff'].agg(['min', 
+                                                                                              'max', 
+                                                                                              'median'])).reset_index()
+                    self.med_time = med_time[med_time[type_value] < time_treshold*3600]
+                elif less_or_more == ">":
+                    med_time = pd.DataFrame(self.df.groupby('transact')['time_diff'].agg(['min', 
+                                                                                              'max', 
+                                                                                              'median'])).reset_index()
+                    self.med_time = med_time[med_time[type_value] > time_treshold*3600]
+                else:
+                    return 'Недопустимое значение для параметра less_or_more или неправильно указано значение All'
+            except Exception as e:
+                print(e)
+                print('Параметр time_treshold принимает либо значение All, либо  должно иметь тип integer,\
+                совместно с параметром less_or_more, который принимает одно из следующих значений: "<" или ">"')
+    
+    @staticmethod
+    def change_width(secs):
+        if secs <= 1:
+            width = '1'
+        elif (secs > 1) and (secs <= 60):
+            width = '2'
+        elif (secs > 60) and (secs <= 3600):
+            width = '3'
+        elif (secs > 3600) and (secs <= 36000):
+            width = '4'
+        elif secs > 36000:
+            width = '5'
+        return width
+
+    @staticmethod
+    def change_color(secs):
+        if secs <= 1:
+            color = 'cyan'
+        elif (secs > 1) and (secs <= 60):
+            color = 'deepskyblue1'
+        elif (secs > 60) and (secs <= 3600):
+            color = 'goldenrod'
+        elif (secs > 3600) and (secs <= 36000):
+            color = 'coral1'
+        elif secs > 36000:
+            color = 'brown'
+        return color
+    
+    @staticmethod
+    def secondsToText(secs):
+        if secs > 1:
+            days = round(secs//86400)
+            hours = round((secs - days*86400)//3600)
+            minutes = round((secs - days*86400 - hours*3600)//60)
+            seconds = round(secs - days*86400 - hours*3600 - minutes*60)
+            result = ("{}d:".format(days) if days else "") + \
+            ("{}h:".format(hours) if hours else "") + \
+            ("{}m:".format(minutes) if minutes else "") + \
+            ("{}s:".format(seconds))
+        else:
+            result = str(round(secs, 4)) +' '+ 'sec'
+        return result
+    
+    def draw_perform(cls, time_treshold = 'All', type_value = 'median', less_or_more = '>'):
+        cls.time_calculate(time_treshold, type_value, less_or_more)
+        cls.graph_init()
+        cls.check_colon()
+        
+        list_trans = cls.med_time['transact'].values
+        times = cls.med_time['median'].values
+        for c in range(len(list_trans)):
+            tr = list_trans[c]
+            time = float(times[c])
+            start = tr.split('-->')[0]
+            end = tr.split('-->')[1]
+            cls.graph.edge('{0}'.format(start), '{0}'.format(end), 
+                   label='{0}'.format(cls.secondsToText(time)), 
+                   arrowhead='vee', 
+                   penwidth=cls.change_width(time), 
+                   color=cls.change_color(time),
+                   fontcolor=cls.change_color(time))
+        cls.graph.view()        
+        
 
 def draw_frequency_graph(df_prepared, name_file = None, count_treshold = 'All', less_or_more = None):
     
@@ -321,9 +455,9 @@ def draw_frequency_graph(df_prepared, name_file = None, count_treshold = 'All', 
     
     print('Если всплывает много предупреждений, подсвеченных красным (данный вывод некорректен),\
     значит в имени события содержится двоеточие, которое нужно заменить на тире')
-    f.view()
-
-
+    f.view()        
+        
+        
 def draw_performance_graph(df_prepared, name_file = None, time_treshold = "All", less_or_more = None, type_value = 'median'):
     """
     df_prepared - DataFrame с 3 столбцами ['case_name', 'transact', 'time_diff'].
